92e14b037c231c1c9cfc7adfd96642074f33ca92
ShubhamKumar1729 <shubh900688@gmail.com>
2026-04-17 03:08:48 +0530

    fix google oauth callback

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ passport.deserializeUser(User.deserializeUser());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: "https://wandera-1ivj.onrender.com/auth/google/callback"
 },
 async (accessToken, refreshToken, profile, done) => {
     try {
